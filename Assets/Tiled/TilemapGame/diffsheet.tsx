a95ea886e1347fc0b87d9d42fc5210b0e26043c9
OBecchio <oliver.becchio@students.mq.edu.au>
2025-05-07 18:15:55 +1000

    coliders

--- Assets/Tiled/TilemapGame/diffsheet.tsx
+++ Assets/Tiled/TilemapGame/diffsheet.tsx
@@ -1,4 +1,30 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="diff" tilewidth="16" tileheight="16" tilecount="132" columns="12">
  <image source="kenney_tiny-dungeon/Tilemap/tilemap_packed.png" width="192" height="176"/>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="-0.0426382" width="16" height="16.0426"/>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="63">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="105">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0" width="8" height="16"/>
+  </objectgroup>
+ </tile>
 </tileset>
