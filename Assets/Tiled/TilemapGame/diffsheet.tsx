6af2445f1a131a18f370fc48f4164d03c67d1fa3
OBecchio <oliver.becchio@students.mq.edu.au>
2025-05-07 18:54:03 +1000

    health to be added

--- Assets/Tiled/TilemapGame/diffsheet.tsx
+++ Assets/Tiled/TilemapGame/diffsheet.tsx
@@ -2,29 +2,60 @@
 <tileset version="1.10" tiledversion="1.11.2" name="diff" tilewidth="16" tileheight="16" tilecount="132" columns="12">
  <image source="kenney_tiny-dungeon/Tilemap/tilemap_packed.png" width="192" height="176"/>
  <tile id="36">
+  <properties>
+   <property name="unity;layer" value="Default"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="37">
+  <properties>
+   <property name="unity:layer" value="Default"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
    <object id="2" x="0" y="-0.0426382" width="16" height="16.0426"/>
+   <object id="3" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="38">
+  <properties>
+   <property name="unity:layer" value="Default"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="63">
+  <properties>
+   <property name="unity:layer" value="Default"/>
+  </properties>
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
  <tile id="105">
   <objectgroup draworder="index" id="2">
    <object id="1" x="4" y="0" width="8" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="124">
+  <properties>
+   <property name="unity:IsTrigger" value="true"/>
+   <property name="unity:layer" value="hazard"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16">
+    <properties>
+     <property name="unity:IsTrigger" value="true"/>
+     <property name="unity:layer" value="hazard"/>
+    </properties>
+   </object>
   </objectgroup>
  </tile>
 </tileset>
