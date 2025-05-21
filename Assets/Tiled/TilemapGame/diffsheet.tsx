85faa0b59f270cae71a94d1f009f86dc305caf1f
OBecchio <oliver.becchio@students.mq.edu.au>
2025-05-21 18:21:51 +1000

    gfd

--- Assets/Tiled/TilemapGame/diffsheet.tsx
+++ Assets/Tiled/TilemapGame/diffsheet.tsx
@@ -1,6 +1,20 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="diff" tilewidth="16" tileheight="16" tilecount="132" columns="12">
  <image source="kenney_tiny-dungeon/Tilemap/tilemap_packed.png" width="192" height="176"/>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16">
+    <properties>
+     <property name="unity:layer" value="Default"/>
+    </properties>
+   </object>
+   <object id="2" x="0" y="0" width="16" height="16">
+    <properties>
+     <property name="unity:layer" value="Default"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
  <tile id="36">
   <properties>
    <property name="unity;layer" value="Default"/>
@@ -27,6 +41,19 @@
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
    <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <properties>
+   <property name="unity:layer" value="Default"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16">
+    <properties>
+     <property name="unity:layer" value="Default"/>
+    </properties>
+   </object>
   </objectgroup>
  </tile>
  <tile id="63">
